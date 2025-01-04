37dfb1696d78099db9aa380a39c236f8ae2f86b0
Ali Saif <145758021+AliSaif1@users.noreply.github.com>
2025-01-05 03:42:49 +0500

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,7 +1,1 @@
-import mongoose from 'mongoose';
 
-// const mongoUri = 'mongodb+srv://sabirrizwan321:vrKhbsCvg1Sj4gBI@cluster0.q3akg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const mongoUri = 'mongodb+srv://alisaif2617:Ali11297@fyp.you7dai.mongodb.net/influencerHarbor?retryWrites=true&w=majority&appName=fyp';
-mongoose.connect(mongoUri)
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch((error) => console.error('Error connecting to MongoDB:', error));
